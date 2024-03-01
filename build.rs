545acb1b4f9753fcb7eb6db27246201740165765
Carl Lundin <clundin@google.com>
2024-02-29 16:52:26 -0800

    Use bindgen to generate Scalable Auth bindings.

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 use std::env;
+use std::path::PathBuf;
 use std::process::Command;
+
+use bindgen::CargoCallbacks;
 
 fn main() {
     let lib_dir = env::var("OUT_DIR").unwrap();
@@ -23,5 +26,18 @@ fn main() {
         .unwrap();
 
     println!("cargo:rustc-link-search=native={}", &go_library_root);
-    println!("cargo:rustc-link-lib=static=scalable-auth");
+    println!("cargo:rustc-link-lib=static=scalable_auth");
+
+    let headers_path_str = format!("{go_library_root}/libscalable_auth.h");
+
+    let bindings = bindgen::Builder::default()
+        .header(headers_path_str)
+        .parse_callbacks(Box::new(CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bindings.rs");
+    bindings
+        .write_to_file(out_path)
+        .expect("Couldn't write bindings!");
 }
--- src/access_token.rs
+++ src/access_token.rs
@@ -1,64 +1,76 @@
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(unused)]
+#![allow(improper_ctypes)] // This is because of transitive go dependencies in the header file. Not
+                           // due to the code we use
+
 use anyhow::{anyhow, Result};
 
-use core::ffi::c_int;
-
 use std::ffi::{CStr, CString};
-use std::os::raw::c_char;
+use std::os::raw::{c_char, c_ulong};
 
-// #[link(name = "scalable-auth")]
-extern "C" {
-    fn CreateAccessToken(
-        uri: *const c_char,
-        scopes: *const c_char,
-        token: *mut c_char,
-        token_len: *mut c_int,
-    ) -> c_int;
-}
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[derive(Debug)]
 pub struct AccessToken(String);
 
 impl AccessToken {
     pub async fn from_uri(uri: &str, scopes: &str) -> Result<Self> {
-        let uri = CString::new(uri)?;
-        let scopes = CString::new(scopes)?;
-
-        let mut token_buffer = Vec::new();
-        let mut token_len: c_int = 0;
-
-        let result = unsafe {
-            CreateAccessToken(
-                uri.as_ptr(),
-                scopes.as_ptr(),
-                std::ptr::null_mut(),
-                &mut token_len,
-            )
-        };
-
-        if result == 0 {
-            return Err(anyhow!("Error calling AccessToken"));
-        }
-
-        token_buffer.reserve(token_len as usize);
-
-        let result = unsafe {
-            CreateAccessToken(
-                uri.as_ptr(),
-                scopes.as_ptr(),
-                token_buffer.as_mut_ptr(),
-                &mut token_len,
-            )
-        };
-
-        if result == 0 {
-            return Err(anyhow!("Error calling AccessToken"));
-        }
-
-        let token_string = unsafe { CStr::from_ptr(token_buffer.as_ptr()) }.to_str()?;
-        Ok(Self(token_string.to_string()))
+        let token = fetch_token(uri, scopes)?;
+        Ok(Self(token))
     }
 
     pub async fn raw_authorization_header(&self) -> Result<(String, String)> {
         Ok(("authorization".to_owned(), format!("Bearer {}", self.0)))
     }
+}
+
+fn fetch_token(uri: &str, scopes: &str) -> Result<String> {
+    let uri = CString::new(uri)?;
+    let scopes = CString::new(scopes)?;
+
+    let token_size = query_token_size(&uri, &scopes)?;
+
+    let mut token_buffer: Vec<c_char> = Vec::with_capacity(token_size);
+    read_token(&uri, &scopes, &mut token_buffer)?;
+
+    let token_string = unsafe { CStr::from_ptr(token_buffer.as_ptr()) }.to_str()?;
+    Ok(token_string.to_owned())
+}
+
+fn query_token_size(uri: &CString, scopes: &CString) -> Result<usize> {
+    let mut token_len: c_ulong = 0;
+    let result = unsafe {
+        CreateAccessToken(
+            uri.as_ptr(),
+            scopes.as_ptr(),
+            std::ptr::null_mut(),
+            &mut token_len,
+        )
+    };
+
+    if result == 0 {
+        return Err(anyhow!("Error calling AccessToken"));
+    }
+
+    Ok(token_len as usize)
+}
+
+fn read_token(uri: &CString, scopes: &CString, token_buf: &mut Vec<c_char>) -> Result<()> {
+    let mut token_len: c_ulong = token_buf.capacity().try_into()?;
+    let result = unsafe {
+        CreateAccessToken(
+            uri.as_ptr(),
+            scopes.as_ptr(),
+            token_buf.as_mut_ptr(),
+            &mut token_len,
+        )
+    };
+
+    if result == 0 {
+        return Err(anyhow!("Error calling AccessToken"));
+    }
+
+    Ok(())
 }
